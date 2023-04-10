f4aee780f4564aa5456e1eabc9db734fa9745b5b
Filipe Pinto <filipe.pinto@globallogic.com>
2023-04-10 15:16:33 +0200

    Final version of the controller

--- src/basic_controller.cpp
+++ src/basic_controller.cpp
@@ -11,19 +11,22 @@ using namespace std::chrono_literals;
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
-class TurtlebotController : public rclcpp::Node
+class TurtlebotControl : public rclcpp::Node
 {
   public:
-    TurtlebotController()
-    : Node("turtlebot_controller") {
+    TurtlebotControl()
+    : Node("turtlebot_control"), m_vel_linear(0.0), m_vel_angular(0.0) {
 
       RCLCPP_INFO(this->get_logger(), "Turtlebot controller node started.");
 
       // Create a publisher object, able to push messages
-      publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+      m_publisher = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
       
+      m_vel_linear = this->declare_parameter("linear_velocity", 0.0);
+      m_vel_angular = this->declare_parameter("angular_velocity", 0.0);
+
       // Create a timer to publish our messages periodically
-      timer_ = this->create_wall_timer(50ms, std::bind(&TurtlebotController::run, this));
+      m_timer = this->create_wall_timer(50ms, std::bind(&TurtlebotControl::run, this));
     }
 
   private:
@@ -31,18 +34,23 @@ class TurtlebotController : public rclcpp::Node
     {
       auto message = geometry_msgs::msg::Twist();
 
-      // TODO: Our Control code goes here 
-      publisher_->publish(message);
+      message.linear.x = m_vel_linear;
+      message.angular.z = m_vel_angular;
+
+      m_publisher->publish(message);
     }
     
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    double m_vel_linear;
+    double m_vel_angular;
+    
+    rclcpp::TimerBase::SharedPtr m_timer;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr m_publisher;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<TurtlebotController>());
+  rclcpp::spin(std::make_shared<TurtlebotControl>());
   rclcpp::shutdown();
 
   return 0;
