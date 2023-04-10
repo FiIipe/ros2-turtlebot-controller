8e52640684c7f9f486b1f984f34fc280af861d84
Filipe Pinto <filipe.pinto@globallogic.com>
2023-04-10 15:16:46 +0200

    Add launch file

---
+++ launch/test_controller_launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    turtlesim_node = Node(
+        package='turtlesim',
+        executable='turtlesim_node',
+        name='turtlesim',
+        output='screen',
+    )
+
+    turtlebot_controller_node = Node(
+        package='turtlebot_control',
+        executable='basic_controller_exe',
+        name='basic_controller_node',
+        output='screen',
+        remappings=[('cmd_vel', 'turtle1/cmd_vel')],
+        parameters=[
+            {"linear_velocity": 1.0},
+            {"angular_velocity": 1.0},
+        ]
+    )
+
+    ld.add_action(turtlesim_node)
+    ld.add_action(turtlebot_controller_node)
+
+    return ld
